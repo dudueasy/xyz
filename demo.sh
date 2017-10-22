0db5109bcf6cd4ac58e307ae41d8470af75c9590
Apolo <apolo@192.168.0.100>
2017-10-23 02:01:23 +0800

    demo for shell

--- demo.sh
+++ demo.sh
@@ -1,1 +1,15 @@
-xx
+cd ~
+mkdir demo
+cd demo
+mkdir css
+mkdir js
+
+# codes below could be omitted
+touch css/style.css
+touch index.html
+# codes above could be omitted
+
+touch js/main.js
+echo -e "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</hi>" > index.html
+echo -e "h1{color: red;}" > css/style.css
+echo -e 'var string = "Hello World"\nalert(string)' > js/main.js
