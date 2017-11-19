fe9f010dfb401182f063407ce6c726ebe8e5f4f8
dudueasy <dudueasy@126.com>
2017-11-19 09:56:08 +0800

    键盘导航页面增加了定制功能, 使用本地存储来保存键位对应网页的信息.

--- JS_keyboard_navigator/js.js
+++ JS_keyboard_navigator/js.js
@@ -65,8 +65,7 @@
         //确保只对hash对象中的key生效, 当key不存在的时候, indexOf 返回值是 -1
         if(Object.keys(hash).indexOf(key) > -1){
             //stop unassigned key from triggering new tab.
-            window.open(hash[key],'_self')
+            window.open(hash[key],'_blank')
         }
     })
 
-    console.log(index)
