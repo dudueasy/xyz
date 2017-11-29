82a1f57cde338d91e1807a8455538b684f5b47fc
dudueasy <dudueasy@126.com>
2017-11-30 00:34:25 +0800

    更新了onscroll事件下顶部条的样式.

--- cv/script/script.js
+++ cv/script/script.js
@@ -1,16 +1,13 @@
 // auto change topNavBar background
-window.onload= function(){window.onscroll=function(){
-    
+window.onload= function(){
+    window.onscroll=function(){
+        var topNavBar = document.querySelector('.topNavBar')
     if (document.documentElement.scrollTop > 0){
-        document.querySelector('.topNavBar').style.background = "white"
-        document.querySelector('.topNavBar nav').style.color = "black"
-        document.querySelector('.topNavBar').classList.add("shadowed")
+            topNavBar.classList.add('sticky')
 
     }
     else {
-        document.querySelector('.topNavBar').style.background = "transparent"
-        document.querySelector('.topNavBar nav').style.color = "rgba(255, 255, 255, 0.7)"
-        document.querySelector('.topNavBar').classList.remove("shadowed")   
+            topNavBar.classList.remove('sticky')
         }
     }
 }
