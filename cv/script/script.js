eaee03a71b619f6ce0c80fd6ac1ff338b4f38228
dudueasy <dudueasy@126.com>
2017-12-30 04:14:17 +0800

    添加了滑动事件的动画效果, 技能进度条的动画效果

--- cv/script/script.js
+++ cv/script/script.js
@@ -2,21 +2,49 @@
 
 setTimeout( ()=> {
     siteWelcome.classList.remove('active')
-}, 2000)
+    onscrollAnimation()
+}, 1000)
 
 window.onscroll=function(){
     var topNavBar = document.querySelector('.topNavBar')
-if (document.documentElement.scrollTop > 0) {
-        topNavBar.classList.add('sticky')
-    }
-else {
-        topNavBar.classList.remove('sticky')
-    }
+    if (document.documentElement.scrollTop > 0) {
+            topNavBar.classList.add('sticky')
+        }
+    else {
+            topNavBar.classList.remove('sticky')
+        }
+
+    onscrollAnimation()
+
+
 }
+
+function onscrollAnimation() {
+    let specialTags = document.querySelectorAll('[data-x]')
+    let minIndex = 0
+    for(let i =0; i<specialTags.length; i++) {
+        if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)) {
+            minIndex = i
+        }
+    }
+    // minIndex 离页面顶部最近的元素的索引
+    specialTags[minIndex].classList.remove('offset')
+    let id = specialTags[minIndex].id
+
+    let a = document.querySelector('a[href="#' + id +'"]')
+    let li = a.parentNode
+    let liSiblings = li.parentNode.children
+
+    for(let i =0; i<liSiblings.length; i++){
+        liSiblings[i].classList.remove('highlight')
+    }
+
+    li.classList.add('highlight')
+}
+
 
 window.onload = function () {
     let liTags = document.querySelectorAll('nav.menu >ul >li')
-
 
     for (let i = 0; i < liTags.length; i++) {
         liTags[i].onmouseenter = function (e) {
