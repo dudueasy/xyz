23ca14c42f1ba79608a4c29dbae73245fa33831e
dudueasy <dudueasy@126.com>
2017-12-30 01:12:04 +0800

    Easing scroll feature added!

--- cv/script/script.js
+++ cv/script/script.js
@@ -1,10 +1,14 @@
 // auto change topNavBar background
 
+setTimeout( ()=> {
+    siteWelcome.classList.remove('active')
+}, 2000)
+
 window.onscroll=function(){
     var topNavBar = document.querySelector('.topNavBar')
-if (document.documentElement.scrollTop > 0){
+if (document.documentElement.scrollTop > 0) {
         topNavBar.classList.add('sticky')
-}
+    }
 else {
         topNavBar.classList.remove('sticky')
     }
@@ -14,7 +18,7 @@ window.onload = function () {
     let liTags = document.querySelectorAll('nav.menu >ul >li')
 
 
-    for(let i =0; i<liTags.length; i++){
+    for (let i = 0; i < liTags.length; i++) {
         liTags[i].onmouseenter = function (e) {
             e.currentTarget.classList.add('active')
         }
@@ -24,20 +28,31 @@ window.onload = function () {
         }
     }
 
+    function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+    }
+
+    requestAnimationFrame(animate);
+
     let aTags = document.querySelectorAll('nav.menu > ul > li> a')
-    for(let i =0; i<aTags.length; i++){
+
+    for (let i = 0; i < aTags.length; i++) {
         aTags[i].onclick = function (x) {
             x.preventDefault()
-            let a = x.currentTarget
-            let href = a.getAttribute('href')
-            let element = document.querySelector(href)
-            let top = element.offsetTop
-            top -= 80
-            console.log(top)
-            window.scrollTo(0, top)
+            let top = document.querySelector(x.currentTarget.getAttribute('href')).offsetTop
+            let currentTop = window.scrollY
+            let targetTop = top - 80
 
+            let coords = { y: currentTop };
+            let tween = new TWEEN.Tween(coords)
+                .to({ y: targetTop }, 500)
+                .easing(TWEEN.Easing.Quadratic.In)
+                .onUpdate(function() {
+                    window.scrollTo(0, coords.y)
+                })
+                .start();
         }
     }
 }
-
 
