32bb5d04e5c64f780caf8832d95bfbfb04d68dce
dudueasy <dudueasy@126.com>
2018-01-09 20:47:12 +0800

    调整了代码结构

--- slides/Loop mode/main.js
+++ slides/Loop mode/main.js
@@ -1,33 +1,40 @@
-let n
-initiallize()
-setInterval(()=>{
-    if(n>3){n-=3}
-    displayNode(getImage(n))
-    let next = (n%3)+1
-    moveAwayNode(getImage(next))
-    n+=1
-}, 1000)
+    let n
+    var timerId
+    initiallize()
+    startLoop()
 
+    function initiallize() {
+        n = 1
+        $(`.images> img:nth-child(${n})`).addClass('displaying')
+            .siblings().addClass('queuing')
+    }
 
+    function getImage(order) {
+        return $(`.images > img:nth-child(${order})`)
+    }
 
-function initiallize(){
-    n =1
-    $(`.images> img:nth-child(${n})`).addClass('displaying')
-        .siblings().addClass('queuing')
-}
+    function moveAwayNode($node) {
+        $node.removeClass('displaying').addClass('left').one(
+            'transitionend', (e) => {
+                $(e.currentTarget).removeClass('left').addClass('queuing')
+            }
+        )
+    }
 
-function getImage(order) {
-    return $(`.images > img:nth-child(${order})`)
-}
+    function displayNode($node) {
+        $node.removeClass('queuing').addClass('displaying');
+    }
 
-function displayNode($node) {
-    $node.removeClass('displaying').addClass('left').one(
-        'transitionend',(e)=>{
-            $(e.currentTarget).removeClass('left').addClass('queuing')
-        }
-    )
-}
+    function startLoop() {
+        timerId = setInterval(() => {
+            if (n > 3) {
+                n -= 3
+            }
+            moveAwayNode(getImage(n))
+            let next = (n % 3) + 1
+            displayNode(getImage(next))
+            n += 1
 
-function moveAwayNode($node){
-    $node.removeClass('queuing').addClass('displaying');
-}
+        }, 1000)
+     }
+
