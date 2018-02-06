2f3f1c3d8f681f2a526878bcb5e9478b32b1aa86
dudueasy <dudueasy@126.com>
2018-02-06 23:48:59 +0800

    创建了一个image标签发起请求和JSONP的demo

--- server/nodejs_server_demo/server.js
+++ server/nodejs_server_demo/server.js
@@ -12,37 +12,33 @@ if (!port) {
 }
 
 let server = http.createServer(function (request, response) {
-    let parsedUrl = url.parse(request.url, true)
-    let pathWithQuery = request.url
-    let queryString = ''
-    if (pathWithQuery.indexOf('?') >= 0) {
-        queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
-    }
-    let path = parsedUrl.pathname
-    let query = parsedUrl.query
-    let method = request.method
+        let parsedUrl = url.parse(request.url, true)
+        let pathWithQuery = request.url
+        let queryString = ''
+        if (pathWithQuery.indexOf('?') >= 0) {
+            queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+        }
+        let path = parsedUrl.pathname
+        let query = parsedUrl.query
+        let method = request.method
 
+        console.log('查询字符串的路径为 \n' + pathWithQuery)
+        console.log(method)
 
-    console.log('查询字符串的路径为 \n' + pathWithQuery)
-    console.log(method)
+        // 访问根路径
+        if (path === '/') {
+            response.statusCode = 200
+            response.setHeader('Content-Type', 'text/html;charset=utf-8')
 
-    // 访问根路径
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+            htmlData = getHTMLData('index.html')
+            dbData = getDBData('db.txt')
+            dynamicHtmlData = putDBDataToHTML(htmlData, dbData)
+            response.write(dynamicHtmlData)
+        }
+        // 访问 /pay 路径
+        else if (path === '/pay/') {
 
-        htmlData = getHTMLData('index.html')
-        dbData = getDBData('db.txt')
-        dynamicHtmlData = putDBDataToHTML(htmlData, dbData)
-
-
-        response.write(dynamicHtmlData)
-        response.end()
-    }
-    // 访问 /pay 路径
-    else if (path === '/pay') {
-        if (method.toUpperCase() === 'POST') {
-            {
+            if (method === 'POST') {
                 response.statusCode = 200
                 response.setHeader('Content-Type', 'text/html;charset=utf-8')
 
@@ -52,24 +48,62 @@ let server = http.createServer(function (request, response) {
                 resetDBData('db.txt', Current_Data)
                 dynamicHtmlData = putDBDataToHTML(htmlData, Current_Data)
 
-
                 response.write(dynamicHtmlData)
-                response.end()
             }
+        }
+        // image标签发起请求demo
+        else if (path === '/image_request_demo/') {
+            if (method === 'GET') {
+                data = getHTMLData('request_by_image.html')
+                response.write(data)
+            }
+        }
+        // image 请求处理
+        else if (path === '/image_request/') {
+            if (method === 'GET') {
 
+                // 这里的代码用来响应 <img> 发起的请求
+                response.statusCode = 200
+                response.setHeader('Content-Type', 'image/jpeg')
+
+                data = fs.readFileSync('db_pic2.jpeg')
+                response.write(data)
+            }
+        }
+        // script 请求demo
+        else if (path === '/script_request_demo/') {
+            if (method === 'GET') {
+                data = getHTMLData('request_by_script.html')
+                response.write(data)
+            }
+        }
+        // script 请求处理
+        else if (path === '/script_request/') {
+            if (method === 'GET') {
+                // 设置响应头
+                response.statusCode = 200
+                response.setHeader('Content-Type', 'text/plain;charset=utf-8')
+
+                // 通过查询参数中 callback 的值, 动态地返回回调函数
+                data = `${query.callback}({"msg":"apolo"})`
+
+                // 响应数据
+                response.write(data)
+            }
         }
 
         else {
+            console.log(path)
             response.statusCode = 404
             response.setHeader('Content-Type', 'text/html;charset=utf-8')
             response.write('404 not found')
-            response.end()
         }
-
+        response.end()
     }
 )
 
 //================ 工具代码start here ================
+
 function getHTMLData(path) {
     // readFileSync 必须制定 encoding参数, 才会返回字符串, 否则返回一个 buffer对象
 
@@ -77,8 +111,8 @@ function getHTMLData(path) {
 
 }
 
-function getDBData(path) {
-    return db_data = fs.readFileSync(path, 'utf8')
+function getDBData(path, option = 'utf8') {
+    return db_data = fs.readFileSync(path, option)
 }
 
 function resetDBData(path, data) {
