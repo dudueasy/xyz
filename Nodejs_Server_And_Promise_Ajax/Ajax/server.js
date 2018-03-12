575a262a8a42fcd331b396fcc1cc0276e2233dcd
dudueasy <dudueasy@126.com>
2018-03-13 05:22:33 +0800

    添加了注册页面, 前端表单验证功能

--- Nodejs_Server_And_Promise_Ajax/Ajax/server.js
+++ Nodejs_Server_And_Promise_Ajax/Ajax/server.js
@@ -31,7 +31,17 @@ let server = http.createServer(function (request, response) {
             response.statusCode = 200
             response.setHeader('Content-Type', 'text/html;charset=utf-8')
             htmlData = getHTMLData('index.html')
-            response.write(htmlData)
+            // response.write(htmlData)
+            response.end(htmlData)
+        }
+        //
+        else if (path === '/comic.png') {
+            response.statusCode = 200
+            response.setHeader('Content-Type', 'image/png')
+            htmlData = fs.readFileSync('comic.png', "binary");
+            response.write(htmlData, "binary"); //格式必须为 binary，否则会出错
+
+            response.end()
         }
         // 获取 Customized_Ajax.js文件
         else if (path === '/Customized_Ajax.js') {
@@ -39,6 +49,8 @@ let server = http.createServer(function (request, response) {
             response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
             data = getHTMLData('./Customized_Ajax.js')
             response.write(data)
+            response.end()
+
         }
         // 访问 promise ajax 页面
         else if (path === '/promise/') {
@@ -46,6 +58,8 @@ let server = http.createServer(function (request, response) {
             response.setHeader('Content-Type', 'text/html;charset=utf-8')
             htmlData = getHTMLData('index_promise.html')
             response.write(htmlData)
+            response.end()
+
         }
         // 获取 Customized_Promise_Ajax.js 文件
         else if (path === '/Customized_Promise_Ajax.js') {
@@ -53,6 +67,8 @@ let server = http.createServer(function (request, response) {
             response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
             data = getHTMLData('Customized_Promise_Ajax.js')
             response.write(data)
+            response.end()
+
         }
         // 获取一个 json 串作为响应
         else if (path === '/xxx') {
@@ -70,6 +86,8 @@ let server = http.createServer(function (request, response) {
                     }
                 })
             )
+            response.end()
+
         }
         // 访问注册页面
         else if (path === '/register') {
@@ -82,7 +100,6 @@ let server = http.createServer(function (request, response) {
             }
             // 注册页面提交数据
             else if (method === 'POST') {
-                let data;
                 getPostData(request).then(
                     (postData) => {
 
@@ -90,30 +107,30 @@ let server = http.createServer(function (request, response) {
                         console.log('postData: ', postData)
                         console.log(email, password, confirm_password)
                         console.log('typeof email: ', typeof email)
-
+                        // 验证失败
                         if (email.indexOf('@') < 0 || password !== confirm_password) {
                             console.log('验证失败')
-                            response.statusCode = 400
                             if (email.indexOf('@') < 0) {
+                                response.statusCode = 400
+
                                 response.setHeader('Content-Type', 'application/json;charset=utf-8')
-                                response.write(`{
-                                    "errors":{
-                                        "email":"invalid"
-                                    }
-                                }`)
+                                response.write(`{"errors":{"email":"invalid"}}`)
+                                response.end()
                             }
                             else if (password !== confirm_password) {
+                                response.statusCode = 400
                                 response.setHeader('Content-Type', 'application/json;charset=utf-8')
-                                response.write(`{
-                                    "errors":{
-                                        "password":"not match"
-                                    }
-                                }`)
+                                response.write(`{ "errors":{ "password":"not match"} }`)
+                                response.end()
                             }
 
                         }
+                        // 验证成功
                         else {
-                            console.log('验证成功')
+                            response.statusCode = 200
+                            response.setHeader('Content-Type', 'application/json;charset=utf-8')
+                            response.write(`"验证成功"`)
+                            response.end()
                         }
 
 
@@ -125,10 +142,7 @@ let server = http.createServer(function (request, response) {
 
                     }
                 )
-
-
             }
-
         }
         // 访问登陆页面
         else if (path === '/sign_in') {
@@ -136,15 +150,19 @@ let server = http.createServer(function (request, response) {
             response.setHeader('Content-Type', 'text/html;charset=utf-8')
             data = getHTMLData('sign_in.html')
             response.write(data)
+            response.end()
+
 
         }
-        else {
+
+        // 如果访问的是其他路径
+        else if (path) {
             console.log(path)
             response.statusCode = 404
             response.setHeader('Content-Type', 'text/html;charset=utf-8')
             response.write('404 not found')
+            response.end()
         }
-        response.end()
     }
 )
 
